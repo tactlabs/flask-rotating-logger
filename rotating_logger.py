5d4d2af8fb7e4297cb358cfceed62fe226946a3c
Raja CSP Raman <raja.csp@gmail.com>
2019-10-06 12:16:32 -0400

    Set level updated

--- rotating_logger.py
+++ rotating_logger.py
@@ -21,24 +21,29 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# 1 MB
+MAX_BYTES = 1000000
+
 @app.route('/')
 def start():
     app.logger.warning('A warning occurred (%d apples)', 77)
-    app.logger.error('An error occurred')
-    app.logger.info('Info')
+    app.logger.error('Error')
+    app.logger.info('Info printed')
     
     content = """
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    Korum Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
     """
     
-    for x in range(10000):
+    for x in range(4):
         print(x, content)
-        app.logger.error(str(x) + content)
+        app.logger.info(str(x) + content)
     
     return "start"
 
 if __name__ == '__main__':
-    handler = RotatingFileHandler('p:/test/start.log', maxBytes=1000000, backupCount=100)
-    handler.setLevel(logging.INFO)
+    handler = RotatingFileHandler('/Users/rajacsp/tactlogs/tout.log', maxBytes=MAX_BYTES, backupCount=100)
+
+    logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+    #handler.setLevel(logging.DEBUG)
     app.logger.addHandler(handler)
     app.run()
